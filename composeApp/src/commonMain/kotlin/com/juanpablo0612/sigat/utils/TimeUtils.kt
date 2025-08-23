fbb869e168ccd64f186f16948ae3ace4367906c0
JuanPablo0612 <gallegojuanpablo39@gmail.com>
2025-08-23 12:55:31 -0500

    chore: downgrade kotlinx-datetime to version 0.6.2 and clean up imports in TimeUtils

--- composeApp/src/commonMain/kotlin/com/juanpablo0612/sigat/utils/TimeUtils.kt
+++ composeApp/src/commonMain/kotlin/com/juanpablo0612/sigat/utils/TimeUtils.kt
@@ -1,5 +1,6 @@
 package com.juanpablo0612.sigat.utils
 
+import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.format
@@ -7,7 +8,7 @@ import kotlinx.datetime.format.FormatStringsInDatetimeFormats
 import kotlinx.datetime.format.byUnicodePattern
 import kotlinx.datetime.toLocalDateTime
 import kotlin.time.ExperimentalTime
-import kotlin.time.Instant
+
 
 @OptIn(FormatStringsInDatetimeFormats::class, ExperimentalTime::class)
 fun timestampToDayMonthYearFormat(timestamp: Long): String {
